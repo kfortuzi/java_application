c94769b0c6693193b829a51b80632f9d2b18aa36
User <User@LAPTOP-G7SLQEQV>
2022-07-04 18:27:39 +0200

    final commit

--- src/main/java/com/mycompany/javaprojecttest1/JavaProjectTest1.java
+++ src/main/java/com/mycompany/javaprojecttest1/JavaProjectTest1.java
@@ -13,5 +13,10 @@ public class JavaProjectTest1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        System.out.println("krenar fortuzi");
+        
+        
     }
 }
